77f0204dbdebfcf0121db402880ae17c142b913a
Kirill <ktoropov59@gmail.com>
2023-07-13 21:17:32 +0800

    Updates

--- assets/redirectToLogin-89f0e500.js
+++
@@ -1,1 +0,0 @@
-import{a as r,j as n,N as s,F as a}from"./index-960eff3d.js";let c=t=>({isAuth:a(t)});const m=t=>{const o=e=>e.isAuth==!1?n.jsx(s,{to:"/login"}):n.jsx(t,{...e});return r(c,{})(o)};export{m as r};
